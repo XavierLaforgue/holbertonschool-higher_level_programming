a68bcd49c8863d2cccdd8455f7b581a68faa12ac
XavierLaforgue <xavierjimmys@gmail.com>
2025-05-07 01:46:18 +0200

    Create 6-print_comb3.py
    Print all two-diferent-digits numbers separated by commas and terminated by a new line. Use no more than 2 loops and 3 prints with str.format

---
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+for i in range(0, 9):
+    for j in range(1 + i, 10):
+        if (i != j) and not (i == 8 and j == 9):
+            print("{}{}, ".format(i, j), end="")
+print("{}{}".format(8, 9))
