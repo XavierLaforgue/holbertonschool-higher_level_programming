212af41079905ecfe00d7c3de034de80a2dfef70
XavierLaforgue <xavierjimmys@gmail.com>
2025-05-07 00:30:42 +0200

    Update 1-last_digit.py
    Correct usage of modulo operator to handle negative numbers

--- python-if_else_loops_functions/1-last_digit.py
+++ python-if_else_loops_functions/1-last_digit.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
-last = abs(number) % 10
+last = number % 10 if number >= 0 else number % -10
+# last = int(str(number)[-1]) if number >= 0 else -int(str(number)[-1])
 if last > 5:
     msg = "and is greater than 5"
 elif last == 0:
