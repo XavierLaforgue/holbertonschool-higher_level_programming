a9292987aa44097466d1d13d3f2bf91999f5baca
XavierLaforgue <xavierjimmys@gmail.com>
2025-05-06 16:33:41 +0200

    Create 0-positive_or_negative.py
    Print if random number is positive, negative, or zero

---
+++ python-if_else_loops_functions/0-positive_or_negative.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+import random
+number = random.randint(-10, 10)
+if number > 0:
+	message = "is positive"
+elif number == 0:
+	message = "is zero"
+elif number < 0:
+	message = "is negative"
+else:
+	message = "is not a number"
+print(f"{number} {message}")
