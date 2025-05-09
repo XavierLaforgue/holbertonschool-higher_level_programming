0455182c37075bed37fa16910065c64fcbecc38e
XavierLaforgue <xavierjimmys@gmail.com>
2025-05-09 16:15:10 +0200

    Create 103-fast_alphabet.pyand upper_alphabet.py
    Create program that prints the alphabet in uppercase without using loops, string literals, conditional statements, system calls, str.join, and with a maximum length of 3 lines

---
+++ python-import_modules/103-fast_alphabet.py
@@ -0,0 +1,3 @@
+#!/usr/bin/python3
+from upper_alphabet_103 import str
+print(f"{str}")
---
+++ python-import_modules/upper_alphabet_103.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+str = ""
+for char in range(ord("A"), ord("Z")):
+    str += chr(char)
+if __name__ == "__main__":
+    print(str)
