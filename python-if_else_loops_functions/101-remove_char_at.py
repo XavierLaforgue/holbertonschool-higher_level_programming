32dc27d5d40e2e9d2f23b4e8a2fddc94a299d722
XavierLaforgue <xavierjimmys@gmail.com>
2025-05-09 12:58:33 +0200

    Create 101-main.py and 101-remove_char_at.py
    Create function that creates a copy of a string and remove a character at a certain position

---
+++ python-if_else_loops_functions/101-remove_char_at.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def remove_char_at(str, n):
+    str2 = ""
+    for i in range(0, len(str)):
+        if i != n:
+            str2 += str[i]
+        else:
+            str2 += " "
+    return str2
