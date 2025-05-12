5133d2f119c802613aa4ee0c415ceffad3ba2742
XavierLaforgue <xavierjimmys@gmail.com>
2025-05-12 16:35:59 +0200

    Update 7-add_tuple.py
    Change to cleaner method

--- python-data_structures/7-add_tuple.py
+++ python-data_structures/7-add_tuple.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def add_tuple(tuple_a=(), tuple_b=()):
-    x, y = 0, 0
+    """ x, y = 0, 0
     if len(tuple_a) > 0:
         x += tuple_a[0]
     if len(tuple_b) > 0:
@@ -8,5 +8,11 @@ def add_tuple(tuple_a=(), tuple_b=()):
     if len(tuple_a) > 1:
         y += tuple_a[1]
     if len(tuple_b) > 1:
-        y += tuple_b[1]
+        y += tuple_b[1] """
+    a = tuple_a[0:2]
+    b = tuple_b[0:2]
+    a += (2 - len(a)) * (0,)
+    b += (2 - len(b)) * (0,)
+    x = a[0] + b[0]
+    y = a[1] + b[1]
     return x, y
