2874cf0f48184d9bf04412c4ef755730060bf024
XavierLaforgue <xavierjimmys@gmail.com>
2025-05-13 15:04:39 +0200

    Update 6-print_sorted_dictionary.py
    Comment away unnecessary code

--- python-more_data_structures/6-print_sorted_dictionary.py
+++ python-more_data_structures/6-print_sorted_dictionary.py
@@ -4,7 +4,9 @@ def print_sorted_dictionary(a_dictionary):
         print("{}: {}".format(a, b))
     # sorting key-value pairs by key and by value if keys ar the same
     # also capital and lower case letters are considered to be equivalent:
+    """
     for i in sorted(
             {"a": 2, "d": 4, "h": 4, "B": 5, "p": 1000, "c": 3}.items(),
             key=lambda kv: (kv[0].lower(), kv[1])):
         print(i)
+    """
