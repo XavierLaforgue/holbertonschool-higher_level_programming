135d34952e896146940e234fd45c5545197b152d
XavierLaforgue <xavierjimmys@gmail.com>
2025-05-13 03:17:55 +0200

    Update 2-uniq_add.py
    Remove spurious test prints

--- python-more_data_structures/2-uniq_add.py
+++ python-more_data_structures/2-uniq_add.py
@@ -2,7 +2,6 @@
 def uniq_add(my_list=[]):
     new_list = my_list.copy()
     for i in range(len(my_list) - 1, -1, -1):
-        print(i, my_list[i])
         if new_list.count(new_list[i]) > 1:
             del new_list[i]
     sum = 0
