958edfb40d5ce27a25ffcfc0ce34b0f8c5bc2c45
XavierLaforgue <xavierjimmys@gmail.com>
2025-05-13 15:02:49 +0200

    Create 6-main.py and 6-print_sorted_dictionary.py
    Create function that prints a dictionary by ordered keys

---
+++ python-more_data_structures/6-print_sorted_dictionary.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def print_sorted_dictionary(a_dictionary):
+    for a, b in sorted(a_dictionary.items()):
+        print("{}: {}".format(a, b))
+    # sorting key-value pairs by key and by value if keys ar the same
+    # also capital and lower case letters are considered to be equivalent:
+    for i in sorted(
+            {"a": 2, "d": 4, "h": 4, "B": 5, "p": 1000, "c": 3}.items(),
+            key=lambda kv: (kv[0].lower(), kv[1])):
+        print(i)
