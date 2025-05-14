c8a18ed7b8877b25ad22d5bfd29cef255c3d11d0
XavierLaforgue <xavierjimmys@gmail.com>
2025-05-15 01:30:41 +0200

    Update 0-safe_print_list.py
    Do not try to use the iterating variable outside of the for loop

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -3,7 +3,7 @@ def safe_print_list(my_list=[], x=0):
     try:
         for i in range(x):
             print("{}".format(my_list[i]), end="")
-        return i+1
+        return x
     except IndexError:
         return i
     finally:
