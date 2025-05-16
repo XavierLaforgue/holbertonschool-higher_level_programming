7f38a969b2efee6c47bb5a5be96413c7914bb383
XavierLaforgue <xavierjimmys@gmail.com>
2025-05-16 11:06:13 +0200

    Update 4-print_square.py and tests/4-print_square.txt
    Fix typo of ValueError message

--- python-test_driven_development/4-print_square.py
+++ python-test_driven_development/4-print_square.py
@@ -28,6 +28,6 @@ def print_square(size):
     if not isinstance(size, int):
         raise TypeError('size must be an integer')
     if size < 0:
-        raise ValueError('size must be >=0')
+        raise ValueError('size must be >= 0')
     for height in range(size):
         print('#' * size)
