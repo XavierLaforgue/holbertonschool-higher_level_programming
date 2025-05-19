f1ff88266960af580a570e97ead1ad9718a73722
XavierLaforgue <xavierjimmys@gmail.com>
2025-05-19 11:20:54 +0200

    Create 0-square.py and 0-main.py
    Create an empty class Square that defines a square

---
+++ python-classes/0-square.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+Module Name: 0-square.py
+
+Contains an empty class named Square.
+
+Class(es):
+    Square: defines a square.
+"""
+
+
+class Square:
+    """
+    Creates an empty class Square that defines a square without
+    attributes nor procedures
+    """
+    pass
