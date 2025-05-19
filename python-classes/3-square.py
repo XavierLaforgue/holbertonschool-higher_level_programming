97a7bada573fb495ed0f45b55c36d0a7ab0b5219
XavierLaforgue <xavierjimmys@gmail.com>
2025-05-19 13:58:11 +0200

    Create 3-square.py and 3-main.py
    Create class Square that defines a square by a private instance attribute size, which is optional and a positive integer, and a public instance method area that rturns the area of the current square

---
+++ python-classes/3-square.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+"""
+Module Name: 1-square.py
+
+Contains only one class to define an object named square and
+characterize it with its size
+
+Classes:
+    Square - defines a square and size as one of its attributes
+"""
+
+
+class Square(object):
+    """
+    Creates a Square class of objects which is defined with a size.
+
+    Attributes:
+        size - private attribute for the square.
+    """
+    def __init__(self, size=0):
+        """
+        Initialize instance of square with optional size argument.
+
+        Args:
+            size - int
+        """
+        if not isinstance(size, int):
+            raise TypeError('size must be an integer')
+        if size < 0:
+            raise TypeError('size must be >= 0')
+        self.__size = size
+
+    def area(self):
+        """
+        Calculates the area of the square.
+
+        Returns:
+            area of the square, an int.
+        """
+        return self.__size**2
