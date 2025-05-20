383eb6d0c798184c0f9fdefd8fbda10d46d1348e
XavierLaforgue <xavierjimmys@gmail.com>
2025-05-20 14:38:08 +0200

    Create 0-rectangle.py and 0-main.py
    Create an empty class Rectangle that defines a recangle without any attributes nor methods

---
+++ python-more_classes/0-rectangle.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+Module name: 0-rectangle.py
+
+Contains one empty class called Rectangle.
+
+Classes:
+    Rectangle: does nothing.
+"""
+
+
+class Rectangle(object):
+    """
+    Creates an emoty class Rectangle that defines a Rectangle without
+    any attributes or methods.
+    """
+    pass
