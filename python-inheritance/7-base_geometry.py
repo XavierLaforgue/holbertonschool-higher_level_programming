90eb13664bdeb6bb2c9ab5d4769012aba4e3e119
XavierLaforgue <xavierjimmys@gmail.com>
2025-05-26 15:14:33 +0200

    Update tests/7-base_geometry.txt
    Add tests for value: None, list, bool, set, tuple

--- python-inheritance/7-base_geometry.py
+++ python-inheritance/7-base_geometry.py
@@ -30,7 +30,7 @@ class BaseGeometry:
 
     def integer_validator(self, name, value):
         """Validate input for integer field/attribute."""
-        if not isinstance(value, int):
+        if type(value).__name__ != int.__name__:
             raise TypeError(f"{name} must be an integer")
         if value <= 0:
             raise ValueError(f"{name} must be greater than 0")
