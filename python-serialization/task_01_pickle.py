79f4ffdd9a9ee8d5db5795d0754c68bebc1a3564
XavierLaforgue <xavierjimmys@gmail.com>
2025-06-05 01:45:20 +0200

    Create task_01_pickle.py
    Create custom class with methods to get pickled and unpickled

---
+++ python-serialization/task_01_pickle.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+"""
+Module Name: task_01_pickle.
+
+Contain a custom class with a few methods including one to pickle
+itself.
+"""
+import pickle
+import os
+
+
+class CustomObject(object):
+    """Define a Custom Object."""
+
+    def __init__(self, name: str, age: int, is_student: bool):
+        """Instantiate Custom Object with attributes."""
+        self.name = name
+        self.age = age
+        self.is_student = is_student
+
+    def display(self):
+        """Display the attributes of the custom object."""
+        if self is not None:
+            print(f"Name: {self.name}\nAge: {self.age}\n"
+                  f"Is Student: {self.is_student}")
+        
+    def serialize(self, filename: str):
+        """Serialize class into file using picle."""
+        with open(filename, "wb") as f:
+            pickle.dump(self, f)
+
+    @classmethod
+    def deserialize(cls, filename: str) -> "CustomObject | None":
+        if os.path.exists(filename):
+            with open(filename, "rb") as f:
+                loaded = pickle.load(f)
+            if isinstance(loaded, CustomObject):
+                return loaded
+        return None
