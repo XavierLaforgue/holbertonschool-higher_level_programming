6797a1a4bbcd171261db2418c533409084242e0b
XavierLaforgue <xavierjimmys@gmail.com>
2025-06-25 12:00:32 +0200

    Update 8-cities_of_california.sql
    Sort the results by cities id in ascending order

--- SQL_more_queries/8-cities_of_california_subquery.sql
+++ SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,3 +1,6 @@
 -- List all cities of California in hbtn_0d_usa database
-SELECT * FROM cities WHERE state_id=(SELECT id FROM states WHERE name='California');
+SELECT * FROM cities WHERE state_id=(
+	SELECT id FROM states WHERE name='California'
+	)
+	ORDER BY id ASC;
  
