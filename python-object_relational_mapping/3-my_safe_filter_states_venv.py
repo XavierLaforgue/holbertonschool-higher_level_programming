ada87fe061fed85bce117c8653b112b09b194866
XavierLaforgue <xavierjimmys@gmail.com>
2025-06-26 14:25:47 +0200

    Create 3-my_safe_filter_states.py
    Create a script that takes an argument and dispplays all values in the states table where name nmatches the argument, but safe from MySQL injections

---
+++ python-object_relational_mapping/3-my_safe_filter_states_venv.py
@@ -0,0 +1,10 @@
+#!./orm-venv/bin/python
+"""
+Module Name: 2-my_filter_states_venv.
+
+Contains execution of my_filter_states function.
+"""
+if __name__ == "__main__":
+    my_filter_states = __import__('2-my_filter_states').my_filter_states
+
+    my_filter_states()
