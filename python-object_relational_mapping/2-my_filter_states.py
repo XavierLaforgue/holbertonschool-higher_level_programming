7157a8306108c53ff566d949636955e6edc49b01
XavierLaforgue <xavierjimmys@gmail.com>
2025-06-26 12:38:03 +0200

    Create 2-my_filter_states.py
    Create script that takes in an argument and displays all values in the states table of the hbtn_0e_0_usa database where name matches the argument

---
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+"""
+Module Name: 2-my_filter_states.
+
+Contains a script that takes an argument and displays all values in the
+states table of hbtn_0e_0_usa where name matches the argument.
+"""
+from sys import argv
+import MySQLdb
+
+
+def my_filter_states():
+    """List values for which states matches the desired state name."""
+    mysql_username = argv[1]
+    mysql_password = argv[2]
+    db_name = argv[3]
+    state_name = argv[4]
+
+    db = MySQLdb.connect(
+        user=mysql_username,
+        passwd=mysql_password,
+        database=db_name,
+        host='localhost',
+        port=3306)
+    c = db.cursor()
+    c.execute("""SELECT * FROM states
+            WHERE states.name=%s
+            ORDER BY states.id ASC""", (state_name,))
+    for row in c.fetchall():
+        print(row)
+
+    c.close()
+    db.close()
+
+
+if __name__ == "__main__":
+    my_filter_states()
