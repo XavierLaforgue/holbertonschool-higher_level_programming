d2c310a036042eb2763dd30db93443154eccfcd5
XavierLaforgue <xavierjimmys@gmail.com>
2025-06-26 12:23:19 +0200

    Update 1-filter_states.py
    Add BINARY to LIKE statement so only capital N is matched, and put script back into a function

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -5,11 +5,12 @@ Module name: 1-filter_states.
 Contains something a script that lists the states whose names start
 with the capital letter 'N'.
 """
-if __name__ == "__main__":
-    from sys import argv
-    import MySQLdb
+from sys import argv
+import MySQLdb
 
-    # def filter_states():
+
+def filter_states():
+    """List states whose name start with capital N."""
     mysql_username = argv[1]
     mysql_password = argv[2]
     db_name = argv[3]
@@ -22,7 +23,7 @@ if __name__ == "__main__":
         port=3306)
     c = db.cursor()
     c.execute("""SELECT * FROM states
-            WHERE states.name LIKE %s
+            WHERE states.name LIKE BINARY %s
             ORDER BY states.id ASC""", ("N%",))
     for row in c.fetchall():
         print(row)
@@ -30,4 +31,6 @@ if __name__ == "__main__":
     c.close()
     db.close()
 
-    # filter_states()
+
+if __name__ == "__main__":
+    filter_states()
--- python-object_relational_mapping/1-filter_states_venv.py
+++ python-object_relational_mapping/1-filter_states_venv.py
@@ -5,6 +5,5 @@ Module name: 1-filter_states_venv.
 Contains something.
 """
 if __name__ == "__main__":
-    from sys import argv
     filter_states = __import__('1-filter_states').filter_states
     filter_states()
