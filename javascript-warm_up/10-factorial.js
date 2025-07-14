2076db2c79f10a130ff4726430e1cd2606c24ee7
XavierLaforgue <xavierjimmys@gmail.com>
2025-07-14 23:59:24 +0200

    10-factorial.js create
    factorial

---
+++ javascript-warm_up/10-factorial.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+function factorial (n) {
+  if (isNaN(n)) {
+    return 1;
+  } else if (n === 0 || n === 1) {
+    return 1;
+  }
+  return n * factorial(n - 1);
+}
+
+const num = Number(process.argv[2]);
+console.log(factorial(num));
