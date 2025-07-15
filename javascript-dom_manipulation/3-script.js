8595e3a4786b82a012a0fa8a3686c20f1582ebd1
XavierLaforgue <xavierjimmys@gmail.com>
2025-07-16 00:21:26 +0200

    3-script.js create
    Toggle classes

---
+++ javascript-dom_manipulation/3-script.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+const header = document.querySelector('header');
+const toggleHeader = document.querySelector('#toggle_header');
+toggleHeader.addEventListener('click', () => {
+  header.classList.toggle('green');
+  header.classList.toggle('red');
+  }
+);
