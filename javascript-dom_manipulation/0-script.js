b9f67f1534aa0a16c42e2901b508dff263a3a699
XavierLaforgue <xavierjimmys@gmail.com>
2025-07-15 18:33:53 +0200

    0-script.js update
    Semistandard

--- javascript-dom_manipulation/0-script.js
+++ javascript-dom_manipulation/0-script.js
@@ -1,3 +1,3 @@
 #!/usr/bin/node
-let header = document.querySelector("header");
-console.log(header.style.color="#FF0000");
+const header = document.querySelector('header');
+console.log(header.style.color = '#FF0000');
