b77e56dcd5733ac05039a321b5359d67c6e1a477
XavierLaforgue <xavierjimmys@gmail.com>
2025-07-15 23:23:27 +0200

    2-script.js create
    Add .red class

---
+++ javascript-dom_manipulation/2-script.js
@@ -0,0 +1,6 @@
+#!/usr/bin/node
+const header = document.querySelector('header');
+const redHeader = document.querySelector('#red_header');
+redHeader.addEventListener('click', () => {
+  header.classList.add('red');
+});
