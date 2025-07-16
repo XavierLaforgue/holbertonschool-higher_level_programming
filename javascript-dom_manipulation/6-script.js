5b63d7a434cbe11be88a6ce0399c15e582ecac0d
XavierLaforgue <xavierjimmys@gmail.com>
2025-07-16 02:14:58 +0200

    6-script.js create
    Add name of star wars character

---
+++ javascript-dom_manipulation/6-script.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+const url = 'https://swapi-api.hbtn.io/api/people/5/?format=json';
+async function fetchName(url) {
+  const response = await fetch(url);
+  const responseJson = await response.json();
+  return responseJson.name;
+}
+fetchName(url).then(name => {
+  const character = document.querySelector('#character');
+  character.append(name);
+});
